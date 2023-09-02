34a6c9723f73f0ee0da053050c47d1363fd9e0ae
Zia Ur Rehman <ziaurrehman@Zias-MacBook-Air.local>
2023-09-02 14:48:38 +0500

    delete build file

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -34,13 +34,7 @@ return (
           </div>
 
           {showUserMenu && (
-            <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700">
-                Your Profile
-              </a>
-              <a href="#" className="block px-4 py-2 text-sm text-gray-700">
-                Settings
-              </a>
+            <div className="absolute right-0 z-10 mt-2 w-auto origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <LogoutButton />
             </div>
           )}
